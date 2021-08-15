3f58841411474543763c82f0531bde61f88f37d2
mangxiao <mangxiao2018@126.com>
2021-08-15 20:07:23 +0800

    sentinel-springboot-sample

---
+++ sentinel-springboot-sample/src/main/java/com/mangxiao/sample/sentinel/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.mangxiao.sample.sentinel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ sentinel-springboot-sample/src/main/java/com/mangxiao/sample/sentinel/controller/SentinelSampleApplication.java
@@ -0,0 +1,12 @@
+package com.mangxiao.sample.sentinel.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelSampleApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SentinelSampleApplication.class, args);
+    }
+}
